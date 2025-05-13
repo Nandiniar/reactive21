5ccdc7f5b60dd56f17a61cbb5de3607b467f0962
nandini <nandani@contaque.com>
2025-05-13 09:58:53 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactice21';
+  name=new FormControl('Ram'); // aise karne se default value aayege bracket ke andar likhne se
+  password=new FormControl(123);
+  displayvalue(){
+    console.log(this.name.value);
+    console.log(this.password.value);
+  }
+  setValues(){
+    this.name.setValue('ABC');
+    this.password.setValue(12345);
+
+  }
 }
